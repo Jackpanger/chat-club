031112e0eb2259817b2835dff7a0c68e769250ad
Jack Pang <jp6862@nyu.edu>
2024-03-09 20:07:40 -0500

    Basic Skeleton

---
+++ chat-club-subject/chat-club-starter/src/main/java/com/jackpang/subject/SubjectApplication.java
@@ -0,0 +1,19 @@
+package com.jackpang.subject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * description: SubjectApplication
+ * date: 3/9/24 7:49 PM
+ * author: jinhao_pang
+ * version: 1.0
+ */
+@SpringBootApplication
+@ComponentScan("com.jackpang")
+public class SubjectApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SubjectApplication.class);
+    }
+}
