13e3352d9f9194d6690df2899dbde5e54e8562c8
Jack Pang <jp6862@nyu.edu>
2024-03-22 23:04:27 -0400

    feat: skeleton of authorization

--- chat-club-auth/chat-club-auth-application/chat-club-auth-application-controller/src/main/java/com/jackpang/auth/application/controller/UserController.java
+++ chat-club-auth/chat-club-auth-application/chat-club-auth-application-controller/src/main/java/com/jackpang/auth/application/controller/UserController.java
@@ -1,7 +1,11 @@
 package com.jackpang.auth.application.controller;
 
+import cn.dev33.satoken.stp.SaTokenInfo;
 import cn.dev33.satoken.stp.StpUtil;
+import cn.dev33.satoken.util.SaResult;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,15 +13,32 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     // 测试登录，浏览器访问： http://localhost:8081/user/doLogin?username=zhang&password=123456
+    @RequestMapping("hello")
+    public String hello() {
+        return "Hello, Sa-Token!";
+    }
     @RequestMapping("doLogin")
-    public String doLogin(String username, String password) {
-        // 此处仅作模拟示例，真实项目需要从数据库中查询数据进行比对 
+    public SaResult doLogin(@RequestParam("username") String username, @RequestParam("password") String password) {
+        // 此处仅作模拟示例，真实项目需要从数据库中查询数据进行比对
         if("zhang".equals(username) && "123456".equals(password)) {
             StpUtil.login(10001);
-            return "登录成功";
+            SaTokenInfo tokenInfo = StpUtil.getTokenInfo();
+            // 第3步，返回给前端
+            return SaResult.data(tokenInfo);
         }
-        return "登录失败";
+        return SaResult.error("登录失败");
     }
+    // 登录接口
+//    @RequestMapping("doLogin")
+//    public SaResult doLogin() {
+//        // 第1步，先登录上
+//        StpUtil.login(10001);
+//        // 第2步，获取 Token  相关参数
+//        SaTokenInfo tokenInfo = StpUtil.getTokenInfo();
+//        // 第3步，返回给前端
+//        return SaResult.data(tokenInfo);
+//    }
+
 
     // 查询登录状态，浏览器访问： http://localhost:8081/user/isLogin
     @RequestMapping("isLogin")
