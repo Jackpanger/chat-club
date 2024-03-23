dd1ef0423cf9fd7b0389ebac29c829ebe428393a
Jack Pang <jp6862@nyu.edu>
2024-03-22 21:21:21 -0400

    feat: skeleton of authorization

---
+++ chat-club-auth/chat-club-auth-application/chat-club-auth-application-controller/src/main/java/com/jackpang/auth/application/config/GlobalConfig.java
@@ -0,0 +1,33 @@
+package com.jackpang.auth.application.config;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.List;
+
+/**
+ * description: GlobalConfig mvc global config
+ * date: 3/15/24 2:36 AM
+ * author: jinhao_pang
+ * version: 1.0
+ */
+@Configuration
+public class GlobalConfig extends WebMvcConfigurationSupport {
+    @Override
+    protected void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        super.configureMessageConverters(converters);
+        converters.add(mappingJackson2HttpMessageConverter());
+    }
+
+    private MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        return new MappingJackson2HttpMessageConverter(objectMapper);
+    }
+}
