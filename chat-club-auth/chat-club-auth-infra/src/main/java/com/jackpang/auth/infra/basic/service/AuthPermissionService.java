305ddea44bf7669a79b92793959d1ff27caccd09
Jack Pang <jp6862@nyu.edu>
2024-03-24 14:50:10 -0400

    auth permission skeleton

---
+++ chat-club-auth/chat-club-auth-infra/src/main/java/com/jackpang/auth/infra/basic/service/AuthPermissionService.java
@@ -0,0 +1,53 @@
+package com.jackpang.auth.infra.basic.service;
+
+import com.jackpang.auth.infra.basic.entity.AuthPermission;
+
+/**
+ * (AuthPermission)表服务接口
+ *
+ * @author makejava
+ * @since 2024-03-24 14:49:51
+ */
+public interface AuthPermissionService {
+
+    /**
+     * 通过ID查询单条数据
+     *
+     * @param id 主键
+     * @return 实例对象
+     */
+    AuthPermission queryById(Long id);
+    
+    /**
+     * 查询数据
+     *
+     * @param authPermission 实例对象
+     * @return 实例对象
+     */
+    List<AuthPermission> queryAllByLimit(AuthPermission authPermission);
+    
+    /**
+     * 新增数据
+     *
+     * @param authPermission 实例对象
+     * @return 实例对象
+     */
+    AuthPermission insert(AuthPermission authPermission);
+
+    /**
+     * 修改数据
+     *
+     * @param authPermission 实例对象
+     * @return 实例对象
+     */
+    AuthPermission update(AuthPermission authPermission);
+
+    /**
+     * 通过主键删除数据
+     *
+     * @param id 主键
+     * @return 是否成功
+     */
+    boolean deleteById(Long id);
+
+}
---
+++ chat-club-auth/chat-club-auth-infra/src/main/java/com/jackpang/auth/infra/basic/service/impl/AuthPermissionServiceImpl.java
@@ -0,0 +1,76 @@
+package com.jackpang.auth.infra.basic.service.impl;
+
+import com.jackpang.auth.infra.basic.entity.AuthPermission;
+import com.jackpang.auth.infra.basic.dao.AuthPermissionDao;
+import com.jackpang.auth.infra.basic.service.AuthPermissionService;
+import org.springframework.stereotype.Service;
+
+import jakarta.annotation.Resource;
+
+/**
+ * (AuthPermission)表服务实现类
+ *
+ * @author makejava
+ * @since 2024-03-24 14:49:51
+ */
+@Service("authPermissionService")
+public class AuthPermissionServiceImpl implements AuthPermissionService {
+    @Resource
+    private AuthPermissionDao authPermissionDao;
+
+    /**
+     * 通过ID查询单条数据
+     *
+     * @param id 主键
+     * @return 实例对象
+     */
+    @Override
+    public AuthPermission queryById(Long id) {
+        return this.authPermissionDao.queryById(id);
+    }
+    /**
+     * 查询数据
+     *
+     * @param authPermission 实例对象
+     * @return 实例对象
+     */
+    @Override
+    public List<AuthPermission> queryAllByLimit(AuthPermission authPermission) {
+        return this.authPermissionDao.queryAllByLimit(authPermission);
+    }
+
+    /**
+     * 新增数据
+     *
+     * @param authPermission 实例对象
+     * @return 实例对象
+     */
+    @Override
+    public AuthPermission insert(AuthPermission authPermission) {
+        this.authPermissionDao.insert(authPermission);
+        return authPermission;
+    }
+
+    /**
+     * 修改数据
+     *
+     * @param authPermission 实例对象
+     * @return 实例对象
+     */
+    @Override
+    public AuthPermission update(AuthPermission authPermission) {
+        this.authPermissionDao.update(authPermission);
+        return this.queryById(authPermission.getId());
+    }
+
+    /**
+     * 通过主键删除数据
+     *
+     * @param id 主键
+     * @return 是否成功
+     */
+    @Override
+    public boolean deleteById(Long id) {
+        return this.authPermissionDao.deleteById(id) > 0;
+    }
+}
